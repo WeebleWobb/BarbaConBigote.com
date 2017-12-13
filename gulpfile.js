488e8c28afde0bf67349d0840a93acf7c6aaa92f
Juan D. Bolaños <bolanosjd@gmail.com>
2017-12-13 15:57:41 -0500

    New task addition

--- gulpfile.js
+++ gulpfile.js
@@ -4,9 +4,13 @@ var
 	gulp = require('gulp'),
 	newer = require('gulp-newer'),
 	imagemin = require('gulp-imagemin'),
-
-	// development mode?
-	devBuild = (process.env.NODE_ENV !== 'production'),
+	htmlclean = require('gulp-htmlclean');
+	concat = require('gulp-concat'),
+  	deporder = require('gulp-deporder'),
+  	stripdebug = require('gulp-strip-debug'),
+  	uglify = require('gulp-uglify'),
+  	purify = require('gulp-purify-css'),
+  	cleanCSS = require('gulp-clean-css');
 
 	// folders
 	folder = {
@@ -15,7 +19,7 @@ var
 	}
 ;
 
-// Image Minification
+// Image Optimization
 gulp.task('images', function() {
 	var imgDest = folder.build + 'assets/';
 	var imgSrc = folder.src + 'assets/**/*';
@@ -24,4 +28,32 @@ gulp.task('images', function() {
 		.pipe(newer(imgDest))
 		.pipe(imagemin({optimizationLevel: 5}))
 		.pipe(gulp.dest(imgDest));
+});
+
+// HTML Minification
+gulp.task('html', ['images'], function() {
+	var
+		htmlDest = folder.build,
+		html = gulp.src(folder.src + '*.html')
+			.pipe(newer(htmlDest));
+
+	return html.pipe(gulp.dest(htmlDest));
+
+});
+
+// JS Minification
+gulp.task('js', function() {
+	return gulp.src(folder.src + 'js/**/*')
+		.pipe(deporder())
+		.pipe(concat('main.js'))
+		.pipe(gulp.dest(folder.build + 'js/'));
+
+});
+
+// CSS clean up and minification
+gulp.task('css', ['images'], function() {
+	return gulp.src(folder.src + 'css/*')
+		.pipe(purify([folder.src + 'js/*.js', folder.src + '*.html']))
+		.pipe(cleanCSS({compatibility: 'ie8'}))
+    	.pipe(gulp.dest(folder.build + 'css/')); 
 });
