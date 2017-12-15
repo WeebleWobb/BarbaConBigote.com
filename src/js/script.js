5a1ac2ddf93a3ddbad5459227c0c43ca84b255ee
Juan D. Bolaños <bolanosjd@gmail.com>
2017-12-15 15:49:17 -0500

    Initial dev + favicon and meta tags + homepage dev

---
+++ src/js/script.js
@@ -0,0 +1,34 @@
+/*!
+* Barba con Bigote v1.0
+* @license MIT licensed
+*
+* Copyright (C) 2017 barbaconbigote.com - A project by Juan D. Bolaños
+*/
+
+$(document).ready(function() {
+
+
+	// --------------------- 01. Navigation --------------------- //
+
+	$('#menu').on('click', function() {
+
+		var $nav = $('#mainNav')
+		var $body = $('body')
+
+
+		// Adds transitions to navigation when clicked
+		$nav.toggleClass('is-open');
+		$('#mainNav .nav-item, #mainNav .contact').toggleClass('is-open');
+		$('#menu').toggleClass('is-open');
+
+		// Prevents site scrolling when navigation is open
+		if($nav.hasClass('is-open')) {
+			$body.css({'overflow': 'hidden'});
+		} else {
+			$body.css({'overflow': 'auto'});
+		}
+	});
+
+
+});
+
