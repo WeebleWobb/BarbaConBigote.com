16cb1dfbe069288cb63baf322a5ad4126126c438
Juan D. Bolaños <bolanosjd@gmail.com>
2018-01-09 16:28:06 -0500

    Added repositories pull into github..php + curl API data pull init

--- src/inc/github.php
+++ src/inc/github.php
@@ -1,17 +1,60 @@
-<?php
-$username = 'WeebleWobb';
-$url = 'https://api.github.com/users/weeblewobb';
-$process = curl_init($url);
 
-curl_setopt($process, CURLOPT_USERAGENT, $username);
-curl_setopt($process, CURLOPT_RETURNTRANSFER, 1);
+<!-- Github API -->
+<!-- <?php
+//$username = 'WeebleWobb';
+//$url = 'https://api.github.com/users/weeblewobb';
+//$process = curl_init($url);
 
-$return = curl_exec($process);
-$results = json_decode($return);
+//curl_setopt($process, CURLOPT_USERAGENT, $username);
+//curl_setopt($process, CURLOPT_RETURNTRANSFER, 1);
 
-var_dump($results);
+//$return = curl_exec($process);
+//$results = json_decode($return);
 
-curl_close();
+//var_dump($results);
 
+//curl_close();
+?> -->
 
-?>
+<div class="col-4">
+	<article class="repos-post">
+		<a href="#"><h6>NarcolepsyLink-Email</h6></a>
+		<p>Email development for Narcolepsy link</p>
+		<div class="repos-post-meta">HTML</div>
+	</article>
+</div>
+<div class="col-4">
+	<article class="repos-post">
+		<a href="#"><h6>Synchrony-Email-Signatures</h6></a>
+		<p>A microsite that allows Synchrony employees to easily create their email signatures.</p>
+		<div class="repos-post-meta">HTML</div>
+	</article>
+</div>
+<div class="col-4">
+	<article class="repos-post">
+		<a href="#"><h6>Synchrony-Registration-Platform</h6></a>
+		<p>Event registration platform built by Synchrony Group</p>
+		<div class="repos-post-meta">HTML</div>
+	</article>
+</div>
+<div class="col-4">
+	<article class="repos-post">
+		<a href="#"><h6>SynchronyGroup.com</h6></a>
+		<p>Website Redesign for SynchronyGroup.com</p>
+		<div class="repos-post-meta">HTML</div>
+	</article>
+</div>
+<div class="col-4">
+	<article class="repos-post">
+		<a href="#"><h6>XYREM-AgencyOfChange-Emails</h6></a>
+		<p>XYREM Agency of Change Digital Campaign</p>
+		<div class="repos-post-meta">HTML</div>
+	</article>
+</div>
+<div class="col-4">
+	<article class="repos-post">
+		<a href="#"><h6>CSL-Behring</h6></a>
+		<p>Sharepoint SCP site for CSL Behring</p>
+		<div class="repos-post-meta">HTML</div>
+	</article>
+</div>
--- src/page-work.php
+++ src/page-work.php
@@ -85,50 +85,8 @@
 				Will be pulled from Git Hub API 
 			**************************************************-->
 
-			<?php include('github.php'); ?>
+			<?php include('inc/github.php'); ?>
 
-			<div class="col-4">
-				<article class="repos-post">
-					<a href="#"><h6>NarcolepsyLink-Email</h6></a>
-					<p>Email development for Narcolepsy link</p>
-					<div class="repos-post-meta">HTML</div>
-				</article>
-			</div>
-			<div class="col-4">
-				<article class="repos-post">
-					<a href="#"><h6>Synchrony-Email-Signatures</h6></a>
-					<p>A microsite that allows Synchrony employees to easily create their email signatures.</p>
-					<div class="repos-post-meta">HTML</div>
-				</article>
-			</div>
-			<div class="col-4">
-				<article class="repos-post">
-					<a href="#"><h6>Synchrony-Registration-Platform</h6></a>
-					<p>Event registration platform built by Synchrony Group</p>
-					<div class="repos-post-meta">HTML</div>
-				</article>
-			</div>
-			<div class="col-4">
-				<article class="repos-post">
-					<a href="#"><h6>SynchronyGroup.com</h6></a>
-					<p>Website Redesign for SynchronyGroup.com</p>
-					<div class="repos-post-meta">HTML</div>
-				</article>
-			</div>
-			<div class="col-4">
-				<article class="repos-post">
-					<a href="#"><h6>XYREM-AgencyOfChange-Emails</h6></a>
-					<p>XYREM Agency of Change Digital Campaign</p>
-					<div class="repos-post-meta">HTML</div>
-				</article>
-			</div>
-			<div class="col-4">
-				<article class="repos-post">
-					<a href="#"><h6>CSL-Behring</h6></a>
-					<p>Sharepoint SCP site for CSL Behring</p>
-					<div class="repos-post-meta">HTML</div>
-				</article>
-			</div>
 		</div>
 	</section>
 	<!-- End of Repositories -->
