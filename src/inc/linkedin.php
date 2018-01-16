a6f28ae1103eae156e9d1188f968ecf2404c3db4
Juan D. Bolaños <bolanosjd@gmail.com>
2018-01-16 13:42:25 -0500

    Repeater row dev + Expereience content

--- src/inc/linkedin.php
+++ src/inc/linkedin.php
@@ -16,48 +16,19 @@
 ?>
 
 <!-- About Content will be pulled from LinkedIn API -->
-<div class="row about-detail-content">
-	<aside class="col-4 about-content-left">
-		<p>Aug 2016 - Present</p>
-		<p>1 yr 4 mos</p>
-	</aside>
-	<article class="col-8 about-content-right">
-		<h6 class="color-red">Interactive Designer</h6>
-		<p>Synchrony Group<br> West Chester, PA</p>
-		<ul>
-			<li>Manage and facilitate day to day digital operations</li>
-			<li>Oversees digital projects and processes</li>
-			<li>Design of interactive and digital projects: [Website Redesigns, Apps, Sales Training]</li>
-			<li>Development in CMS [WordPress]</li>
-			<li>Development in front-end languages and frameworks: [HTML, CSS, JS, Bootstrap, Foundation]</li>
-			<li>Development in back-end languages and methodologies: [PHP, AJAX, JSON, ASP.NET]</li>
-			<li>Management of digital projects and deliverables</li>
-			<li>Understanding and implementation of User Experience techniques and best practices</li>
-			<li>Facilitate client’s product visions by researching, conceiving, wireframing, sketching, prototyping, and mocking up user experiences for digital products.</li>
-			<li>Identify design problems and devise elegant solutions.</li>
-		</ul>
-	</article>
-</div>
-<div class="row about-detail-content">
-	<aside class="col-4 about-content-left">
-		<p>Aug 2016 - Present</p>
-		<p>1 yr 4 mos</p>
-	</aside>
-	<article class="col-8 about-content-right">
-		<h6 class="color-red">Designer</h6>
-		<p>QVC<br> West Chester, PA</p>
-		<ul>
-			<li>Manage and facilitate day to day digital operations</li>
-			<li>Oversees digital projects and processes</li>
-			<li>Design of interactive and digital projects: [Website Redesigns, Apps, Sales Training]</li>
-			<li>Development in CMS [WordPress]</li>
-			<li>Development in front-end languages and frameworks: [HTML, CSS, JS, Bootstrap, Foundation]</li>
-			<li>Development in back-end languages and methodologies: [PHP, AJAX, JSON, ASP.NET]</li>
-			<li>Management of digital projects and deliverables</li>
-			<li>Understanding and implementation of User Experience techniques and best practices</li>
-			<li>Facilitate client’s product visions by researching, conceiving, wireframing, sketching, prototyping, and mocking up user experiences for digital products.</li>
-			<li>Identify design problems and devise elegant solutions.</li>
-		</ul>
-	</article>
-</div>
+<?php if(have_rows('linkedin_content')) : while(have_rows('linkedin_content')) : the_row(); ?>
+
+	<div class="row about-detail-content">
+		<aside class="col-4 about-content-left">
+			<?php the_sub_field('timeline'); ?>
+		</aside>
+		<article class="col-8 about-content-right">
+			<?php the_sub_field('experience_content'); ?>
+		</article>
+	</div>
+<?php endwhile; else : ?>
+	<div class="col-12">
+		<h4 class="text-center">Sorry, something went wrong and the content isn't showing up.</h4>
+	</div>
+<?php endif; ?> 
 <!-- End About Content -->
