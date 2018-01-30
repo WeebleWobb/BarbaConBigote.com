041d93c1752a95f8ce198d1e7d9ee022a5810557
Juan D. Bolaños <bolanosjd@gmail.com>
2018-01-30 09:56:11 -0500

    Removec Filter check in + removed checkin placehodlers

--- src/inc/untappd.php
+++ src/inc/untappd.php
@@ -2,141 +2,60 @@
 	
 	$clientId = 'B443B328112B2CE689823416FBBE03353B9497F0';
 	$clientSecret = 'F6C3122C0EE5975CC3535EDCB908319116651F5D';
-	$url = 'https://api.untappd.com/v4/method_name?client_id=' . $clientId . '&client_secret=' . $clientSecret;
+	$url = 'https://api.untappd.com/v4/user/checkins/WeebleWobb?client_id=' . $clientId . '&client_secret=' . $clientSecret;
 
 	$process = curl_init($url);
 
-?>
+	curl_setopt_array($process, array(
+		CURLOPT_RETURNTRANSFER => 1,
+	));
 
-<!-- Personal Content -->
-<div class="row my-3">
-	<div class="col-12">
-		<h6 class="color-red">Filter Check-Ins</h6>
-	</div>
-	<div class="form-group col-12 col-lg-4">
-		<select class="form-control" id="exampleFormControlSelect1">
-			<option>All Styles</option>
-			<option>2</option>
-			<option>3</option>
-			<option>4</option>
-			<option>5</option>
-		</select>
-	</div>
-	<div class="form-group col-12 col-lg-4">
-		<select class="form-control" id="exampleFormControlSelect1">
-			<option>All Breweries</option>
-			<option>2</option>
-			<option>3</option>
-			<option>4</option>
-			<option>5</option>
-		</select>
-	</div>
-	<div class="form-group col-12 col-lg-4">
-		<select class="form-control" id="exampleFormControlSelect1">
-			<option>All Ratings</option>
-			<option>2</option>
-			<option>3</option>
-			<option>4</option>
-			<option>5</option>
-		</select>
-	</div>
-	</div>
+	$return = curl_exec($process);
+	$results = json_decode($return, true);
+
+	$total = $results["response"]["checkins"]["count"];
+	$checkins = $results["response"]["checkins"]["items"];
+
+	// var_dump($checkins);
+?>
 
 <div class="row about-detail-content">
 	<div class="col-12 col-lg-9">
-		<article class="brew">
-			<header class="brew-head">
-				<div class="media">
-					<img class="mr-3 img-fluid" src="https://placehold.it/90x90">
-					<div class="media-body">
-						<h6 class="color-red">Rare Bourbon County Brand Stout (2010)</h6>
-						<p>Goose Island Beer Co.</p>
-						<p>Stout - American Imperial / Double</p>
-					</div>
-				</div>
-			</header>
-			<div class="brew-body">
-				<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod sed diam nonummy. Lorem ipsum dolor sit amet.</p>
-			</div>
-			<footer class="brew-meta">
-				<ul>
-					<li>13% ABV</li>
-					<li>60 IBU</li>
-					<li>(5.00)</li>
-					<li>Checked-in: 11/26/10</li>
-				</ul>
-			</footer>
-		</article>
-		<article class="brew">
-			<header class="brew-head">
-				<div class="media">
-					<img class="mr-3 img-fluid" src="https://placehold.it/90x90">
-					<div class="media-body">
-						<h6 class="color-red">Rare Bourbon County Brand Stout (2010)</h6>
-						<p>Goose Island Beer Co.</p>
-						<p>Stout - American Imperial / Double</p>
-					</div>
-				</div>
-			</header>
-			<div class="brew-body">
-				<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod sed diam nonummy. Lorem ipsum dolor sit amet.</p>
-			</div>
-			<footer class="brew-meta">
-				<ul>
-					<li>13% ABV</li>
-					<li>60 IBU</li>
-					<li>(5.00)</li>
-					<li>Checked-in: 11/26/10</li>
-				</ul>
-			</footer>
-		</article>
-		<article class="brew">
-			<header class="brew-head">
-				<div class="media">
-					<img class="mr-3 img-fluid" src="https://placehold.it/90x90">
-					<div class="media-body">
-						<h6 class="color-red">Rare Bourbon County Brand Stout (2010)</h6>
-						<p>Goose Island Beer Co.</p>
-						<p>Stout - American Imperial / Double</p>
-					</div>
-				</div>
-			</header>
-			<div class="brew-body">
-				<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod sed diam nonummy. Lorem ipsum dolor sit amet.</p>
-			</div>
-			<footer class="brew-meta">
-				<ul>
-					<li>13% ABV</li>
-					<li>60 IBU</li>
-					<li>(5.00)</li>
-					<li>Checked-in: 11/26/10</li>
-				</ul>
-			</footer>
-		</article>
-		<article class="brew">
-			<header class="brew-head">
-				<div class="media">
-					<img class="mr-3 img-fluid" src="https://placehold.it/90x90">
-					<div class="media-body">
-						<h6 class="color-red">Rare Bourbon County Brand Stout (2010)</h6>
-						<p>Goose Island Beer Co.</p>
-						<p>Stout - American Imperial / Double</p>
-					</div>
-				</div>
-			</header>
-			<div class="brew-body">
-				<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod sed diam nonummy. Lorem ipsum dolor sit amet.</p>
-			</div>
-			<footer class="brew-meta">
-				<ul>
-					<li>13% ABV</li>
-					<li>60 IBU</li>
-					<li>(5.00)</li>
-					<li>Checked-in: 11/26/10</li>
-				</ul>
-			</footer>
-		</article>
+
+		<?php
+
+			foreach($checkins as $checkin) {
+
+				$date = new DateTime($checkin["created_at"]);
+
+				echo '<article class="brew">'
+					.	'<header class="brew-head">'
+					.		'<div class="media">'
+					.			'<img class="mr-3 img-fluid" src="' . $checkin["beer"]["beer_label"] .  '">'
+					.			'<div class="media-body">'
+					.				'<h6 class="color-red">' . $checkin["beer"]["beer_name"] . '</h6>'
+					.				'<p>' . $checkin["brewery"]["brewery_name"] . '</p>'
+					.				'<p>' . $checkin["beer"]["beer_style"] . '</p>'
+					.			'</div>'
+					.		'</div>'
+					.	'</header>'
+					.	'<div class="brew-body">'
+					.		'<p>' . $checkin["checkin_comment"] . '</p>'
+					.	'</div>'
+					.	'<footer class="brew-meta">'
+					.		'<ul>'
+					.			'<li>' . $checkin["beer"]["beer_abv"] . '</li>'
+					.			'<li>' . $checkin["rating_score"] . '</li>'
+					.			'<li> Check-in at: ' . $date->format('M d, Y') . '</li>'
+					.		'</ul>'
+					.	'</footer>'
+					. '</article>';
+
+			}
+
+		?>
 	</div>
+
 	<aside class="col-3 brew-stats-container">
 		<div class="brew-stats">
 			<h6>120</h6>
@@ -159,5 +78,13 @@
 			<p>Photos</p>
 		</div>
 	</aside>
+
+<?php
+
+	curl_close($process);
+
+?>
+
+<!-- Personal Content -->
 </div>
 <!-- End About Content -->
