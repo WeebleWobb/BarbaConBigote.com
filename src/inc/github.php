ee1a9e053fd251b233671f8ee68f8f4b9a31b994
Juan D. Bolaños <bolanosjd@gmail.com>
2018-01-30 14:39:37 -0500

    Reworked github API cURL request + moved cURL close before loop

--- src/inc/github.php
+++ src/inc/github.php
@@ -2,7 +2,7 @@
 <?php
 	$user = 'WeebleWobb';
 	$login = 'WeebleWobb:The#1man14';
-	$url = 'https://api.github.com/user/repos?affiliation=owner&sort=created&per_page=6';
+	$url = 'https://api.github.com/user/repos?affiliation=owner&sort=updated';
 
 	$process = curl_init($url);
 	curl_setopt($process, CURLOPT_USERAGENT, $user);
@@ -10,28 +10,39 @@
 	curl_setopt($process, CURLOPT_RETURNTRANSFER, 1);
 
 	$return = curl_exec($process);
-	$results = json_decode($return);
+	$results = json_decode($return, true);
 
-	echo '<div class="row">'
-		.	 '<div class="col-12 mb-3">'
+	// print_r($results);
+
+	curl_close($process);
+
+	echo '<div class="row mb-3">'
+		.	 '<div class="col-12 col-md-6">'
 		.		 '<h3>Current Repositories (' . count($results) . ')</h3>'
 		.	 '</div>'
+		.	'<div class="col-12 col-md-6 text-right align-self-center">'
+		.		'<a href="https://github.com/WeebleWobb" target="_blank">View GitHub Profile</a>'
+		.	'</div>'
 		. '</div>'
 		. '<div id="repoContainer" class="row">';
+
+		$i = 0;
 
 		foreach($results as $repo) {
 			echo '<div class="col-4 repo-flex">'
 				. 	'<article class="repos-post">'
-				. 		'<a href="'. $repo->html_url .'" target="_blank"><h6>' . $repo->name . '</h6></a>'
-				. 		'<p>' . $repo->description . '</p>'
-				. 		'<div class="repos-post-meta">' . $repo->language . '</div>'
+				. 		'<a href="'. $repo["html_url"] .'" target="_blank"><h6>' . $repo["name"] . '</h6></a>'
+				. 		'<p>' . $repo["description"] . '</p>'
+				. 		'<div class="repos-post-meta">' . $repo["language"] . '</div>'
 				. 	'</article>'
 				. '</div>';
+
+			$i++;
+			if($i == 6) break;
 		}
 		
 	echo '</div>';
 
-	curl_close($process);
 ?>
 
 
