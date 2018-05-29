5a8b3691a48bef79cfe7de030cb9e8023ea55e08
Juan D. Bolanos <bolanosjd@gmail.com>
2018-05-29 11:30:51 -0400

    Casestudy template hard-code dev

---
+++ src/single-case_studies.php
@@ -0,0 +1,36 @@
+<?php get_header(); ?>
+
+<?php if(have_posts()) : while(have_posts()) : the_post(); ?>
+	<main role="main">
+		<!-- Headline -->
+		<section>
+			<div class="container">
+				<div class="row">
+					<div class="col-12 col-md-6 offset-md-3 mt-md-5">
+						<h1 class="text-center color-charcoal py-4"><?php the_title(); ?></h1>
+					</div>
+				</div>
+			</div>
+		</section>
+		<!-- End Headline -->
+
+		<!-- Main Content -->
+		<section>
+			<div class="container">
+				<div class="row">
+					<div class="col-12 col-md-10 offset-md-1">
+						<?php the_content(); ?>
+					</div>
+				</div>
+			</div>
+		</section>
+		<!-- End Main Content -->
+	</main>
+<?php endwhile; else : ?>
+	<div class="col-12 col-md-10 offset-md-1">
+		<h4>Sorry, it seems as there was a mistake.</h4>
+		<h6>I don't have any content to show here.</h6>
+	</div>
+<?php endif?>
+
+<?php get_footer(); ?>
