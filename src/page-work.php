650a8253bb877aee7b2acd1a299ea9d0ac42c6d6
Juan D. Bolanos <bolanosjd@gmail.com>
2018-07-25 14:21:47 -0400

    Case studies removal/updates + Home carousel fix

--- src/page-work.php
+++ src/page-work.php
@@ -17,50 +17,16 @@
 	<section>
 		<div class="container">
 			<div class="row">
-
-				<?php 
-					$args = array(
-						'post_type'		=>		'case_studies'
-					);
-
-					$the_query = new WP_Query($args);
-				?>
-
-				<?php if($the_query->have_posts()) : ?>
-
-					<?php while($the_query->have_posts()) : $the_query->the_post(); ?>
-						<div class="col-12 col-md-6 py-2">
-							<figure class="work">
-								<div class="work-img">	
-									<img src="<?php echo get_the_post_thumbnail_url(); ?>" class="img-fluid" />
-									<div class="work-hover">
-										<a class="btn btn-primary" href="<?php the_permalink(); ?>">View Work</a>
-									</div>
-								</div>
-								<figcaption class="work-caption">
-									<a href="<?php the_permalink(); ?>"><h5><?php the_title(); ?></h5></a>
-									<ul class="color-brown project-roles">
-										<?php 
-											$post_id = get_the_id();
-											$terms = get_the_terms($post_id, 'project_role');
-
-											foreach ($terms as $term) {
-												echo '<li class="role">' . $term->name . '</li>';
-											}
-										?>
-									</ul>
-								</figcaption>
-							</figure>
-						</div>
-					<?php endwhile; ?>
-
-					<?php wp_reset_postdata(); ?>
-
-				<?php else : ?>
-					<div class="col-12">
-						<h4 class="text-center">Sorry, something went wrong and my case studies are showing up.</h4>
+				<div class="col-12 col-md-8 offset-md-2">
+					<div class="casestudy--form-container">
+						<h4>Request Case Studies</h4>
+						<p class="mb-2">If you are interested in viewing my detail design process and casestudies, please complete the form to request. Feel free to view my opther work below.</p>
+						<hr>
+						<?php 
+							Ninja_Forms()->display( 5, false )
+						?>
 					</div>
-				<?php endif; ?>
+				</div>
 				
 			</div>
 
