278c44e46091be7899e66dab6ee99aa860642cd8
Juan D. Bolanos <bolanosjd@gmail.com>
2018-08-20 14:11:29 -0400

    Carousel initial dev

--- src/front-page.php
+++ src/front-page.php
@@ -18,22 +18,59 @@
 
 				<!-- Featured Work Carousel -->
 				<div class="aspot-carousel">
-					<img src="<?php echo get_template_directory_uri(); ?>/assets/home/macbook.png" alt="" class="macbook">
-					<div id="featuredWork" class="carousel slide" data-ride="carousel" data-interval="6000">
-						<div class="carousel-inner">
-
-							<!-- Create Custom Post Type for IMGs -->
-							<div class="carousel-item <?php echo $class; ?>">
-								<div class="work-img">
-									<img class="d-block w-100" src="<?php the_post_thumbnail_url(); ?>" alt="">
-									<div class="work-hover">
-										<a class="btn btn-primary" href="<?php echo get_post_permalink(); ?>">View Work</a>
+					<div class="aspot-carousel--container">
+							<img src="<?php echo get_template_directory_uri(); ?>/assets/home/macbook.png" alt="" class="aspot-carousel--macbook">
+							<div id="featuredWork" class="aspot-carousel--images carousel slide" data-ride="carousel" data-interval="6000">
+								<?php 
+									$args = array(
+										'post_type'		=>		'other_works',
+										'tax_query'		=>		array(
+											array(
+												'taxonomy'	=>	'project_type',
+												'field'		=>	'slug',
+												'terms'		=>	'ux-ui'
+											),
+										),
+										'posts_per_page'		=>		3
+									);
+	
+									$the_query = new WP_Query($args);
+									$posts = $the_query->posts;
+									$current = 0;
+									$class = '';
+								?>
+	
+								<?php if($the_query->have_posts()) : $number = 0; ?>
+	
+									<div class="carousel-inner">
+	
+										<?php while($the_query->have_posts()) : $the_query->the_post(); $current++; ?> 
+											<!-- Write wp_query loop to pull 3 case studies -->
+											<?php
+												if($current == 1) {
+													$class = 'active';
+												} else {
+													$class = '';
+												}
+											?>
+											<div class="carousel-item <?php echo $class; ?>">
+												<div class="aspot-carousel--img">
+													<img class="d-block w-100" src="<?php the_post_thumbnail_url(); ?>" alt="">
+													<div class="work-hover">
+														<a class="btn btn-primary" href="<?php echo get_post_permalink(); ?>">View Work</a>
+													</div>
+												</div>
+											</div>
+										<?php endwhile; ?>
+										
+										<?php wp_reset_postdata(); ?>
+	
 									</div>
-								</div>
+	
+								<?php else : ?>
+									<h6>Sorry, there seems to be an error.</h6>
+								<?php endif; ?>
 							</div>
-							<!-- /End -->
-							
-						</div>
 					</div>
 				</div>
 			</div>
