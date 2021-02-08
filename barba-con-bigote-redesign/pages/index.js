cb98e30f8f2ba64ed217ce1d8a2d1e9a609fdb07
Juan D. Bolaños <juanb@aweber.com>
2021-02-08 08:53:33 -0500

    Global layout structure styling + removal of unneeded css + css to sass change

--- barba-con-bigote-redesign/pages/index.js
+++ barba-con-bigote-redesign/pages/index.js
@@ -1,43 +1,19 @@
 import Head from 'next/head'
-import Link from 'next/link'
 import Layout from '../components/layout'
-import styles from '../styles/layout.module.css'
+import Nav from '../components/nav'
 
 export default function Home() {
   return (
     <Layout>
       <Head>
-        <title>Create Next App</title>
+        <title>Juan D. Bolaños | Barba con Bigote</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <nav>
-        <ul>
-          <li>
-            <Link href="/">
-              <a>Home</a>
-            </Link>
-          </li>
-          <li>
-            <Link href="/cerveza">
-              <a>Cerveza</a>
-            </Link>
-          </li>
-          <li>
-            <Link href="/trabajo">
-              <a>Trabajo</a>
-            </Link>
-          </li>
-          <li>
-            <Link href="/musica">
-              <a>Musica</a>
-            </Link>
-          </li>
-        </ul>
-      </nav>
+      <Nav></Nav>
       
-      <main className={styles.main}>
-        <h1 className={styles.title}>
+      <main >
+        <h1 >
           Barba con Bigote Redesign
         </h1>
 
