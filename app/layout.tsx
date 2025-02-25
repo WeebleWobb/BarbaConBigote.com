42dd34faf742345a79855efb2176e29849066bbe
Juan D. Bolaños <juanb@aweber.com>
2025-02-25 17:41:00 -0500

    Updates typeface structure and adds orelaga one font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,4 @@
-import { Signika_Negative } from 'next/font/google'
+import { Signika_Negative, Orelega_One } from 'next/font/google'
 import './styles/globals.scss'
 import Nav from '@/components/navigation/nav'
 import { defaultMetadata } from '@/config/metadata'
@@ -6,7 +6,15 @@ import { defaultMetadata } from '@/config/metadata'
 const signika = Signika_Negative({ 
   subsets: ['latin'],
   weight: ['400', '600', '700'],
-  display: 'swap'
+  display: 'swap',
+  variable: '--font-signika'
+})
+
+const orelega = Orelega_One({ 
+  subsets: ['latin'],
+  weight: ['400'],
+  display: 'swap',
+  variable: '--font-orelega'
 })
 
 export const metadata = defaultMetadata
@@ -17,8 +25,8 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      <body className={signika.className}>
+    <html lang="en" className={`${signika.variable} ${orelega.variable}`}>
+      <body>
         <div className='flex flex-col justify-between md:justify-start h-screen'>
           <Nav />
           {children}
